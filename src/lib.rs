15baef996928394c551723856ac058f9940bd807
Berm MBA11 <berm@mba11.mac>
2019-12-28 20:34:04 +0700

    Print error

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,6 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
